e14beacfe3b67361332009543d362d3377819559
Cyrus <56502655+CyrusFred@users.noreply.github.com>
2022-07-09 21:37:23 +0800

    Add files via upload

--- GitHubSearchUsers/src/components/List.vue
+++ GitHubSearchUsers/src/components/List.vue
@@ -1,1 +1,71 @@
-1
+<template>
+  <div class="row">
+    <div class="card" v-show="info.users.length" v-for="user in info.users" :key="user.login">
+      <a :href="user.html_url" target="_blank">
+        <img :src="user.avatar_url" style='width: 100px' />
+      </a>
+      <p class="card-text">{{ user.login }}</p>
+    </div>
+    <!-- 展示欢迎词 -->
+    <h1 v-show="info.isFirst">Welcome here for search GitHub Users Info!</h1>
+    <!-- 展示加载中 -->
+    <h1 v-show="info.isLoading">Loading...</h1>
+    <!-- 展示错误信息 -->
+    <h1 v-show="info.errMsg">{{ info.errMsg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      info: {
+        isFirst: true,
+        isLoading: false,
+        errMsg: '',
+        users: []
+      }
+    }
+  },
+  mounted () {
+    this.$bus.$on('updateListData', (dataObj) => {
+      // console.log('我是List组件，收到数据', users)
+      // console.log(dataObj)
+      // this.users = users
+      // this.isFirst = isFirst
+      // this.isLoading = isLoading
+      // this.errMsg = errMsg
+      this.info = { ...this.info, ...dataObj }
+    })
+  }
+}
+</script>
+
+<style scoped>
+.album {
+  min-height: 50rem;
+  /* Can be removed; just added for demo purposes */
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  background-color: #f7f7f7;
+}
+
+.card {
+  float: left;
+  width: 33.333%;
+  padding: .75rem;
+  margin-bottom: 2rem;
+  border: 1px solid #efefef;
+  text-align: center;
+}
+
+.card>img {
+  margin-bottom: .75rem;
+  border-radius: 100px;
+}
+
+.card-text {
+  font-size: 85%;
+}
+</style>
---
+++ GitHubSearchUsers/src/components/Search.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input class="inputChange" type="text" placeholder="Enter the name then you click search button"
+        v-model="keyWord" />&nbsp;
+      <button @click="searchUsers">Search</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Search',
+  data () {
+    return {
+      keyWord: ''
+    }
+  },
+  methods: {
+    searchUsers () {
+      // 请求前更新List的数据
+      this.$bus.$emit('updateListData', { isFirst: false, isLoading: true, errMsg: '', users: [] })
+      axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+        response => {
+          // console.log('请求成功了', response.data.items)
+          console.log('请求成功了')
+          // 请求成功后更新List的数据
+          this.$bus.$emit('updateListData', { isLoading: false, errMsg: '', users: response.data.items })
+        },
+        error => {
+          // 请求失败后更新List的数据
+          // console.log('请求失败了',err.message)
+          this.$bus.$emit('updateListData', { isLoading: false, errMsg: error.message, users: [] })
+        }
+      )
+    }
+  }
+}
+</script>
+<style>
+.inputChange {
+  width: 286px
+}
+</style>
